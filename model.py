837eb19c338b1c161f9ecad64f0735e95bc35ef2
AnhPhong8904 <anhphong892004@gmail.com>
2025-09-11 11:08:24 +0700

    Chuyen model sang B,N,5

--- model.py
+++ model.py
@@ -4,25 +4,48 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
         dim = 32
-        self.features = nn.Sequential(
-            nn.Conv2d(3, dim, 3, stride=2, padding=1),   # 112x112
+        self.backbone = nn.Sequential(
+            nn.Conv2d(3, dim, 3, stride=1, padding=1),  # 224x224
+            nn.Conv2d(dim, dim, 3, stride=1, padding=1),  # 224x224
+            nn.BatchNorm2d(dim),
             nn.ReLU(),
-            nn.MaxPool2d(2), #56x56
-            nn.Conv2d(dim, dim*2, 3, stride=2, padding=1), #28x28
+            nn.MaxPool2d(2),  # 112x112
+            nn.Conv2d(dim, dim*2, 3, stride=1, padding=1),
+            nn.Conv2d(dim*2, dim*2, 3, stride=1, padding=1),
+            nn.BatchNorm2d(dim*2),
             nn.ReLU(),
-            nn.MaxPool2d(2), #14x14
-            nn.Conv2d(dim*2, dim*4, 3, stride=2, padding=1), # 7x7
+            nn.MaxPool2d(2),  # 56x56
+            nn.Conv2d(dim*2, dim*4, 3, stride=1, padding=1),
+            nn.Conv2d(dim*4, dim*4, 3, stride=1, padding=1),
+            nn.BatchNorm2d(dim*4),
             nn.ReLU(),
-            nn.MaxPool2d(2), #3x3
-            nn.Conv2d(dim*4, dim*8, 3, stride=2, padding=1), #2x2
+            nn.MaxPool2d(2),  # 28x28
+            nn.Conv2d(dim*4, dim*8, 3, stride=1, padding=1),
+            nn.Conv2d(dim*8, dim*8, 3, stride=1, padding=1),
+            nn.BatchNorm2d(dim*8),
             nn.ReLU(),
-            nn.Conv2d(dim*8, 5, 1, stride=1, padding=0),  # 5 channels: conf + 4 box coords
-            nn.Sigmoid()
+            nn.MaxPool2d(2),  # 28x28
+            nn.Conv2d(dim*8, dim*16, 3, stride=1, padding=1),
+            nn.Conv2d(dim*16, dim*16, 3, stride=1, padding=1),
+            nn.BatchNorm2d(dim*16),
+            nn.ReLU()
         )
+        self.head = nn.Sequential(
+            nn.Conv2d(dim*16, 5, 1, stride=1) # 5 = 4 bbox + 1 obj confidence
+            )
 
     def forward(self, x):
-        x = self.features(x)   # (B,5,2,2)
-        B,C,H,W = x.shape
-        x = x.permute(0,2,3,1) # (B,H,W,C)
-        x = x.view(B, H*W, C)  # (B,4,5) - 4 anchors, 5 channels each
+        x = self.backbone(x)
+        x = self.head(x)
+        x = x.permute(0, 2, 3, 1)  # [B, H, W, 5]
         return x
+
+
+if __name__ == "__main__":
+    import torch, thop
+    model = SimpleCNN()
+    x = torch.randn(1, 3, 224, 224)
+    y = model(x)
+    print(y.shape)  # [1, 7*7, 5]
+    flops, params = thop.profile(model, inputs=(x,))
+    print(f"FLOPS: {flops/1e9:.2f} GFLOPS, Params: {params/1e6:.2f}M")
